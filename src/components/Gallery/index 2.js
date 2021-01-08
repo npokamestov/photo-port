60626c59719a5e6a297d7aea161f042638a332ea
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-01-08 11:29:21 -0500

    added state control for the contact form

---
+++ src/components/Gallery/index 2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+// import photo from '../../assets/small/commercial/0.jpg';
+
+
+function Gallery({ currentCategory }) {
+
+    const { name, description } = currentCategory;
+
+    return (
+        <section>
+            <h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <PhotoList category={currentCategory.name} />
+        </section>
+    );
+};
+
+export default Gallery;
