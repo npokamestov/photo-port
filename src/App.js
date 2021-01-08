71d92ee636018536b28c1232f1f0d58cda570205
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-01-08 13:46:44 -0500

    added conditional rendering for contact form and maintained SPA functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+
   const [categories] = useState([
     { name: 'commercial', description:'Photos of grocery stores, food trucks, and other commercial projects'},
     { name: 'portraits', description: 'Portraits of people in my life' },
@@ -13,6 +14,7 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -20,11 +22,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm />
-        <Gallery currentCategory={currentCategory} />
-        <About />
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory} />
+            <About />
+          </>
+        ) : (
+          <ContactForm />
+        )}
       </main>
     </div>
   );
